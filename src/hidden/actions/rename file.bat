5cc041d16bf511b8002b89cb94da6a2f52e6d668
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-06-21 20:04:40 +0200

    Renamed folder

--- src/hidden/actions/rename file.bat
+++ src/hidden/actions/rename file.bat
@@ -20,7 +20,11 @@ cd !hidden!
 ren "!name!.lnk" "!newName!.lnk"
 copy "!newName!.lnk" "../!newName!.lnk"
 
-ren "!name!/" "!newName!/"
+echo "!name:~0,-4!..!name:~3,-1!/"
+pause
+exit
+
+ren "!name:~0,-4!..!name:~3,-1!/" "!newName:~0,-4!..!newName:~3,-1!/"
 cd "!newName!"
 ren "!name!.bat" "!newName!.bat"
 cd ..
