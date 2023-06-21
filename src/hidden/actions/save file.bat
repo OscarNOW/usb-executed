3a23e27172f8336feccb0e12d1e41512c3b48bd3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-06-21 17:02:01 +0200

    Implemented save file

---
+++ src/hidden/actions/save file.bat
@@ -0,0 +1,24 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+cd ..
+
+for %%I in (.) do set hidden=%%~nxI
+
+cd ..
+for /R "%cd%" %%F in (*.lnk) do (
+    set name=%%~nxF
+    set name=!name:~0,-4!
+)
+cd !hidden!
+
+cd ..
+if not exist "!name!":
+    echo !name! doesn't exist, can't save
+    pause
+    exit
+cd !hidden!
+
+del "!name!"
+
+cd ..
+copy "!name!" "!hidden!/!name!"
