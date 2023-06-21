a0cf7b92dc351509cc1a734b650eb9581090f52f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-06-21 16:28:40 +0200

    Created rename action

---
+++ src/hidden/actions/rename.bat
@@ -0,0 +1,22 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+cd ..
+
+for %%I in (.) do set hidden=%%~nxI
+
+cd ..
+for /R "%cd%" %%F in (*.lnk) do (
+    set name=%%~nxF
+    set name=!name:~0,-4!
+)
+cd !hidden!
+
+set /p newName="Rename (!name!)->"
+
+cd ..
+del "!name!.lnk"
+cd !hidden!
+
+ren "!name!.lnk" "!newName!.lnk"
+
+ren "!name!" "!newName!"
